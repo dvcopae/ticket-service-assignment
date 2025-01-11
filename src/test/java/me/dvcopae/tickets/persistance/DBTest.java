07e73472bb85ff25f14237e395a8bccafadf4b6c
Copae Danut <d.v.copae@gmail.com>
2025-01-11 21:48:25 +0100

    yeee2

--- src/test/java/me/dvcopae/tickets/persistance/DBTest.java
+++ src/test/java/me/dvcopae/tickets/persistance/DBTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.sql.SQLException;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class DBTest {
