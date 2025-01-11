8cfa86969d29fd388bbe30194ed9f6e7421bbd28
Copae Danut <d.v.copae@gmail.com>
2025-01-11 22:00:58 +0100

    exclude db test from pipeline run

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,6 @@ tasks.test {
     useJUnitPlatform()
 
     if (!project.hasProperty("skipDBTest") || project.findProperty("skipDBTest") != "true") {
-        exclude("**/DBTest.*")
+        exclude("me.dvcopae.tickets.persistance/**")
     }
 }
