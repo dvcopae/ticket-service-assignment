f8f61627192fa32ac720c2dd1413914178e10d9d
Copae Danut <d.v.copae@gmail.com>
2025-01-11 19:06:11 +0100

    add checkstyle and spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
 plugins {
-    id("java")
+    java
+    checkstyle
+    id("com.diffplug.spotless") version "7.0.1"
+}
+
+spotless {
+    java {
+        googleJavaFormat() // google's java style guide
+    }
+}
+
+checkstyle {
+    toolVersion = "10.21.1"
+    configFile = file("${project.projectDir}/config/checkstyle/checkstyle.xml")
 }
 
 group = "me.dvcopae.tickets"
