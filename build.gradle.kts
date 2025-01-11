5bd9eddb9bd17e42c85b2330dda63d705889e5a7
Copae Danut <d.v.copae@gmail.com>
2025-01-11 21:57:42 +0100

    exclude db test from pipeline run

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,6 @@ tasks.test {
     useJUnitPlatform()
 
     if (!project.hasProperty("skipDBTest") || project.findProperty("skipDBTest") != "true") {
-        exclude("**/DBTest.class")
+        exclude("**/DBTest.*")
     }
 }
