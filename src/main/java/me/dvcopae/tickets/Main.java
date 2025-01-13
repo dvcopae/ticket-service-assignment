71f918f72825d8886b66e55b375e5f774ea78fd0
Copae Danut <d.v.copae@gmail.com>
2025-01-13 10:46:25 +0100

    checkstyle

--- src/main/java/me/dvcopae/tickets/Main.java
+++
@@ -1,9 +0,0 @@
-package me.dvcopae.tickets;
-
-public class Main {
-
-  @SuppressWarnings("checkstyle:hideutilityclassconstructor")
-  public static void main(final String[] args) {
-    System.out.println("Hello World!");
-  }
-}
